61288ad737c92a0bafb9d407dc7d071321dd7637
Dimitric <dmitry.kruglikov@gmail.com>
2020-09-03 14:10:06 +0300

    l

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5fa2777ecc26ee549576f70488730515.js"
+  "/precache-manifest.70b40a4c1d5ab648bd3420039c383a2e.js"
 );
 
 self.addEventListener('message', (event) => {

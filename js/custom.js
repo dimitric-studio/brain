11e58c9c3885348f294f0e89ab859790fb7f2e74
Dimitric <dmitry.kruglikov@gmail.com>
2020-11-15 15:02:14 +0200

    1502_15112020

--- js/custom.js
+++ js/custom.js
@@ -52,6 +52,19 @@
    console.log('DISPLAY_MODE_LAUNCH:', displayMode);
  });  
 
+/* On iOS */
+// Detects if device is on iOS 
+const isIos = () => {
+  const userAgent = window.navigator.userAgent.toLowerCase();
+  return /iphone|ipad|ipod/.test( userAgent );
+}
+// Detects if device is in standalone mode
+const isInStandaloneMode = () => ('standalone' in window.navigator) && (window.navigator.standalone);
+
+// Checks if should display install popup notification:
+if (isIos() && !isInStandaloneMode()) {
+  this.setState({ showInstallMessage: true });
+}
 
 openShare = async () => {
  if (navigator && navigator.share) {
